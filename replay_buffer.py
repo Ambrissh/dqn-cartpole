9766c94198225ac6e210e46add06ae8bc3ed4a50
Ambrissh <raghavambrissh@gmail.com>
2026-01-24 11:24:26 +0000

    creating the replay buffer

--- replay_buffer.py
+++ replay_buffer.py
@@ -1,1 +1,16 @@
+from collections import deque 
+import random 
+
+class ReplayBuffer(object):
+  def __init__(self, capacity): 
+    self.buffer=deque([],maxlen=capacity) # creating an empty list 
+
+  def push(self,state,action,reward,next_state,done): 
+    self.buffer.append((state,action,reward,next_state,done)) #taking a new step and storing it 
+  
+  def sample(self,batch_size): 
+    return random.sample(self.buffer,batch_size) #a random sample from all the episodes. 
+
+  def __len__(self): 
+    return len(self.buffer)   #the length of number of memories 
 
